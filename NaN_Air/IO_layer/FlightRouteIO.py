0871d7d3d260af342737649c8f404e43f9f37e1b
Dinkus <robertsnaer901@gmail.com>
2019-12-02 13:37:16 +0000

    forgot to add

--- NaN_Air/IO_layer/FlightRouteIO.py
+++ NaN_Air/IO_layer/FlightRouteIO.py
@@ -5,22 +5,27 @@ FILENAME = '../DataFiles/flightRoutes.csv'
 class FlightRouteIO():
 
     def __init__(self):
+        self.get_instance()
+
+    def get_instance(self):
+        """Get a list of dictionaries"""
         self.open_file()
+        return self.__dictList
+
+    def find_in_file(self, argList):
+        """Search file by specified value(s). Takes in a list of arguments"""
+        pass
 
     def read_from_file(self, csvFile):
+        """Function that reads from file opened in 'open_file'"""
         csvReader = csv.DictReader(csvFile, delimiter=",")
         returnList = []
         # next(csvReader, None)
         for line in csvReader:
-            # print(line[Flight], line[RouteID], line[Country], line[Airport], line[FlightDistance])
             returnList.append(line)
-        return returnList
+        self.__dictList = returnList
 
     def open_file(self):
+        """Function that opens a file"""
         with open(FILENAME, encoding="utf8") as csvFile:
-            myList = self.read_from_file(csvFile)
-            for i in myList:
-                print(i)
-
-
-flightRoute = FlightRouteIO()
+            self.read_from_file(csvFile)
