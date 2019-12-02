af5c5b64e772a66c4556df63d9b33626626d1967
ZlaDaniel <danni539953@gmail.com>
2019-12-02 19:02:39 +0000

    Update View.py

--- NaN_Air/UI_layer/View.py
+++ NaN_Air/UI_layer/View.py
@@ -1,125 +1,36 @@
-#View Data menu
-print('''-----------------------------------------
-        View Data
------------------------------------------
-
-1 View worker
-2 View Airplane
-3 View Flight routes
-4 View Voyages
-5 View Flight
-(Input "b" for back or "m" for main menu)
------------------------------------------''')
-# Worker:
-
-print('''       View Worker
------------------------------------------
-    1 View Pilots
-    2 View Attendants
-    3 View Bosses
-    4 View All Staff''')
-
-
-print('''View Pilots
------------------------------------------
-  1 View Specific Pilot
-  2 View All Pilots''')
-#1
-print('''  - Please Input Staff SSN
-''')
-#right SSN
-print('''Pilot information
------------------------------------------
-Name:
-Number:
-Gmail:
-Licence type:
-Availability:
-''')
-#2
-print('''-----------------------------------------
-1 Sort by Airplane licence
-2 Sort by name
-3 View like its in file''')
-
-
-print('''-----------------------------------------
-        View Attendants
------------------------------------------
-    1 View Specific Attendants
-    2 View All Attendants''')
-
-print('''-----------------------------------------
-        Attendants information
------------------------------------------
-    Name:
-    Number:
-    Gmail:
-    Licence type:
-    Availability:
-''')
-
-#View airplane
-
-print('''-----------------------------------------
-        View Airplane
------------------------------------------
-
-1 View specific airplane
-2 View All Airplanes
-''')
-
-#1
-print('  - Please input Airplane ID: ')
-print('''-----------------------------------------
-        View specific airplane
------------------------------------------
-ID: 
-Plane register:
-Manufacturer:
-Model:
-Status:
-Number of seats:
-Odometer:''')
-
-#2
-print('''-----------------------------------------
-        View All Airplane
------------------------------------------
-1 Sort by Type
-2 Sort by (eh) ''')
-print('''
-        Airplanes
-Type:{} 
-ID: {} 
-Seat Number:{}
-Date when available: {}
-Current Country: {}
-Flight register: {}
-''')
-
-#View Flight routes
-
-print('''-----------------------------------------
-        View Flight Routes
------------------------------------------
-1 Specific Route
-2 All Flight Routes
-''')
-#1
-print('''  - Please input Flight Route ID: ''')
-#if its correct
-print('''-----------------------------------------
-        View Flight Route
------------------------------------------ 
-ID:
-Country: 
-Airport:
-Flight Distance:
-Travel Time:
-Emergency Contact:
-Emergency Number:
-''')
-
-# View Voyages
-#Voyages böllur að neðan
+class View():
+    def viewMenu():
+        print('''View Data
+--------------------------------------------
+  1. View worker
+  2. View Airplane
+  3. View Flight routes
+  4. View Voyages
+  5. View Flight 
+--------------------------------------------''')
+        
+        viewMenuInput = input("Input choice(q to Quit, b for Back): ")
+        viewMenuInput = viewMenuInput.lower()
+        if viewMenuInput == "1":
+            print("")
+            View.viewMenu()
+        elif viewMenuInput == "2":
+            print("")
+            View.viewMenu()
+        elif viewMenuInput == "3":
+            print("")
+            View.viewMenu()
+        elif viewMenuInput == "4":
+            print("")
+            View.viewMenu()
+        elif viewMenuInput == "5":
+            print("")
+            View.viewMenu()
+        elif viewMenuInput == "b":
+            return None
+        elif viewMenuInput == "q":
+            print("Forriti lokað!")
+        else:
+            print("WRONG INPUT, TRY AGAIN")
+            View.viewMenu()
+        return viewMenuInput
