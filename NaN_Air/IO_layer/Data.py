bda3364b3be18cac4772298fa39748261da6a3d4
arnar-steinthors <arnardadi99@hotmail.com>
2019-12-02 09:46:01 +0000

    empty classes

--- NaN_Air/IO_layer/Data.py
+++
@@ -1,1 +0,0 @@
-# hér verður allt tengt því að sækja úr csv skrám og senda það yfir á Processing
--- NaN_Air/logic_layer/Processing.py
+++
@@ -1,1 +0,0 @@
-# hér verður unnið úr öllu sem Data.py sendir okkur, unnið með og sent yfir á interface
