b067bc004722f2ce40731bf5916aa031591884bc
ZlaDaniel <danni539953@gmail.com>
2019-12-02 18:58:37 +0000

    Update ProgramUI.py

---
+++ NaN_Air/UI_layer/ProgramUI.py
@@ -0,0 +1,42 @@
+from create import Create
+from update import Update
+from view import View
+
+class ProgramUI():
+    def loginWindow():
+        print('''NaN Air flights system
+--------------------------------------------
+Input your ID to login''')
+    
+        loginIDinput = input("Input: ")
+        return loginIDinput
+    
+    def mainMenu():
+        print('''Main Menu
+--------------------------------------------
+  1. View Data
+  2. Create Data
+  3. Update Data
+--------------------------------------------''')
+    
+        mainMenuInput = input("Input choice(q to Quit): ")
+        mainMenuInput = mainMenuInput.lower()
+        if mainMenuInput == "1":
+            View.viewMenu()
+            ProgramUI.mainMenu()
+        elif mainMenuInput == "2":
+            Create.createMenu()
+            ProgramUI.mainMenu()
+        elif mainMenuInput == "3":
+            Update.updateMenu()
+            ProgramUI.mainMenu()
+        elif mainMenuInput == "q":
+            print("Forriti lokað!")
+        else:
+            print("WRONG INPUT, TRY AGAIN")
+            print(mainMenuInput)
+            ProgramUI.mainMenu()
+        return mainMenuInput
+    
+    loginWindow()
+    mainMenu()
