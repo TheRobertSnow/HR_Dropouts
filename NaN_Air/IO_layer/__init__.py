1327092b71b49a9b2a3877679b95992db85eea26
Arnar Daði Steinþórsson <arnardadi99@hotmail.com>
2019-12-03 17:08:36 +0000

    conflict fix

--- NaN_Air/IO_layer/__init__.py
+++ NaN_Air/IO_layer/__init__.py
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 __all__ = ['AirplaneIO', 'FlightIO', 'FlightRouteIO', 'VoyageIO', 'WorkerIO']
-# import AirplaneIO
-# import FlightIO
-# import FlightRouteIO
-# import VoyageIO
-# import WorkerIO
-=======
-__all__ = ["AirplaneIO", "FlightIO", "FlightRouteIO", "VoyageIO", "WorkerIO"]
->>>>>>> 34f5655694436993af5f8c3a8d490af70fd097c0
-=======
-__all__ = ['AirplaneIO', 'FlightIO', 'FlightRouteIO', 'VoyageIO', 'WorkerIO']
->>>>>>> d03aad65c30abfcab49244f5b7ac7b4dad3acb78
