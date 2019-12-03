c544746ae060f62a76dcd26144fd5c946e5c79e7
Dinkus <robertsnaer901@gmail.com>
2019-12-03 11:52:50 +0000

    Finished FlightRouteIO.py

---
+++ NaN_Air/IOAPI.py
@@ -0,0 +1,4 @@
+import os
+IO_PATH = "C:\\Users\\Dinkus\\Documents\\HR_Dropouts\\NaN_Air\\IO_layer"
+mypath = os.path.abspath(__file__)
+print(iopath)
--- NaN_Air/IO_layer/FlightRouteIO.py
+++ NaN_Air/IO_layer/FlightRouteIO.py
@@ -17,6 +17,7 @@ class FlightRouteIO():
             for line in csvReader:
                 returnList.append(line)
         self.__dictList = returnList
+        return self.__dictList
 
     def write_flight_route_to_file(self, aList):
         """Method takes in a list of data and writes to file"""
@@ -25,9 +26,24 @@ class FlightRouteIO():
             orderedDict = self.convert_to_dict_with_id(aList)
             self.__dictList.append(orderedDict)
             newList = []
-            newList.append(orderedDict['Flight Route ID'])
+            newList.append(orderedDict['flight route id'])
             [newList.append(i) for i in aList]
             csvWriter.writerow(newList)
+
+    def write_dictList_to_file(self):
+        """Method overwrites file with data from dictList"""
+        with open(FILENAME, 'w', newline='', encoding='utf8') as csvfile:
+            fieldnames = ['flight route id'
+                        ,'country'
+                        ,'airport'
+                        ,'flight distance'
+                        ,'travel time'
+                        ,'emergency contact'
+                        ,'emergency number']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for dictionary in self.__dictList:
+                writer.writerow(dictionary)
 
     def getHighestID(self):
         """This method is only used by 'add_dict_to_list'.
@@ -35,61 +51,54 @@ class FlightRouteIO():
         highestID = 0
         for dictionary in self.__dictList:
             for key, value in dictionary.items():
-                if key == "Flight Route ID":
+                if key == "flight route id":
                     if int(value) > highestID:
                         highestID = int(value)
         return highestID + 1
 
     def convert_to_dict_with_id(self, aList):
+        """Function converts list to dict, generates an ID
+        and assigns it to the dictionary"""
         orderedDict, newId = {}, self.getHighestID()
-        orderedDict['Flight Route ID'] = newId
-        orderedDict['Country'] = aList[0]
-        orderedDict['Airport'] = aList[1]
-        orderedDict['Flight distance'] = aList[2]
-        orderedDict['Travel time'] = aList[3]
-        orderedDict['Emergency contact'] = aList[4]
-        orderedDict['Emergency number'] = aList[5]
+        orderedDict['flight route id'] = newId
+        orderedDict['country'] = aList[0]
+        orderedDict['airport'] = aList[1]
+        orderedDict['flight distance'] = aList[2]
+        orderedDict['travel time'] = aList[3]
+        orderedDict['emergency contact'] = aList[4]
+        orderedDict['emergency number'] = aList[5]
         return orderedDict
 
     def convert_to_dict_no_id(self, aList):
+        """Function converts list to dict but doesn't
+        generate an id since ID should be provided in argument list"""
         orderedDict = {}
-        orderedDict['Flight Route ID'] = aList[0]
-        orderedDict['Country'] = aList[1]
-        orderedDict['Airport'] = aList[2]
-        orderedDict['Flight distance'] = aList[3]
-        orderedDict['Travel time'] = aList[4]
-        orderedDict['Emergency contact'] = aList[5]
-        orderedDict['Emergency number'] = aList[6]
+        orderedDict['flight route id'] = aList[0]
+        orderedDict['country'] = aList[1]
+        orderedDict['airport'] = aList[2]
+        orderedDict['flight distance'] = aList[3]
+        orderedDict['travel time'] = aList[4]
+        orderedDict['emergency contact'] = aList[5]
+        orderedDict['emergency number'] = aList[6]
         return orderedDict
 
     def update_data_in_file(self, aList):
-        """Method takes in list of data for and updates file"""
+        """Method takes in list of data, updates the dictionary list
+        and writes the changes to file"""
         for index, dictionary in enumerate(self.__dictList):
-            if self.__dictList[index]['Flight Route ID'] == aList[0]:
-                orderedDict = convert_to_dict_no_id(aList)
-                self.__dictList[index] = orderedDict
-        print(self.__dictList)
-            # for key, value in dictionary.items():
-            #     if key == aList[0]:
-            #
-        # for index, item in enumerate(self.__dictList):
-        #     if item["Flight Route ID"] == aList[0]:
-        #         for itemNum, item in enumerate(self.__dictList[index]):
-        #             item == aList[itemNum]
-        # with open(FILENAME, 'w', encoding='utf8') as csvFile:
-        #     csvWriter = csv.writer(csvFile)
-        #     print(type(self.__dictList[0]))
-        #     csvWriter.writerow(self.__dictList[0])
-        #     for dictionary in self.__dictList:
-        #         tempList = []
-        #         for value in dictionary.values():
-        #             tempList.append(value)
-        #         csvWriter.writerow(tempList)
+            for key, value in dictionary.items():
+                if key == 'flight route id':
+                    print("We here bro")
+                    if value == aList[0]:
+                        print('key found')
+                        self.__dictList[index][aList[1]] = aList[2]
+                        print(self.__dictList)
+                        self.write_dictList_to_file()
 
-
-flightroutes = FlightRouteIO()
-updateline = [1,"Denmark","Kopenhagen",0,"0:00", "Áslaug Steingrímsdóttir", 3547745010]
-newline = ["Denmark","Reykjavik",0,"0:00", "Áslaug Steingrímsdóttir", 3547745010]
-# print(newline)
-flightroutes.write_flight_route_to_file(newline)
-flightroutes.update_data_in_file(updateline)
+# +++ Test cases +++
+# flightroutes = FlightRouteIO()
+# updateline = ["1","country", "Denmark"]
+# newline = ["Denmark","Reykjavik","0","0:00", "Áslaug Steingrímsdóttir", "3547745010"]
+# # print(newline)
+# flightroutes.write_flight_route_to_file(newline)
+# flightroutes.update_data_in_file(updateline)
