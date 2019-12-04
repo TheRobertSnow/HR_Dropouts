334d89c56f61b53ee6d9a753251c97a7583f4b46
ZlaDaniel <danni539953@gmail.com>
2019-12-04 16:38:11 +0000

    Update ProgramUI.py

--- NaN_Air/UI_layer/ProgramUI.py
+++ NaN_Air/UI_layer/ProgramUI.py
@@ -1,14 +1,13 @@
 from UI_layer import Create
 from UI_layer import View
 from UI_layer import Update
-import UIAPI
 
-class ProgramUI:
+class ProgramUI():
     def __init__(self):
         self.myInstance = Create.Create()
-
+        
     def login(self):
-        print('''NaN Air flights system
+        print('''NaN Air flight system
 --------------------------------------------
 Input your ID to login''')
 
@@ -30,27 +29,17 @@ Input your ID to login''')
             viewMenuOutput = View.View.viewMenu()
             if viewMenuOutput == "b":
                 ProgramUI.mainMenu(self)
-            #elif viewMenuOutput == "q":
-                #return viewMenuOutput
         elif mainMenuInput == "2":
             createMenuOutput = Create.Create.createMenu(self.myInstance)
             if createMenuOutput == "b":
                 ProgramUI.mainMenu(self)
-            #elif createMenuOutput == "q":
-                #return createMenuOutput
-        """elif mainMenuInput == "3":
+        elif mainMenuInput == "3":
             updateMenuOutput = Update.Update.updateMenu()
             if updateMenuOutput == "b":
-                ProgramUI.mainMenu()
-            #elif updateMenuOutput == "q":
-                #return updateMenuOutput
+                ProgramUI.mainMenu(self)
         elif mainMenuInput == "q":
             return mainMenuInput
         else:
             print("Wrong input, try again")
             ProgramUI.mainMenu()
         return mainMenuInput
-
-    loginWindow()
-    mainMenu()"""
-
