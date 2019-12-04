6ebeafd5b456ba1d524924e2b00259e28be7cf69
bergurmg <52974748+bergurmg@users.noreply.github.com>
2019-12-04 11:57:03 +0000

    Update Worker.py

---
+++ NaN_Air/logic_layer/Worker.py
@@ -0,0 +1,16 @@
+
+class Worker():
+    def __init__(self, aList):
+        self.name = aList[0]
+        self.position = aList[1]
+        self.address = aList[2]
+        self.phone = aList[3]
+        self.cellPhone = aList[4]
+        self.email = aList[5]
+        self.active = aList[6]
+        self.available = aList[7]
+        self.planeLicence = aList[8]
+
+
+    def __str__(self): 
+        return ''
