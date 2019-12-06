cdafa548665be0a6762c6422f24562f6e348982d
Bergur Gudmundsson <bergurgudmundsson@Bergurs-MacBook-Pro-2.local>
2019-12-06 10:52:51 +0000

    Very epic

--- NaN_Air/IO_layer/WorkerIO.py
+++ NaN_Air/IO_layer/WorkerIO.py
@@ -1,13 +1,12 @@
 import csv
-import sys
-FILENAME = 'DataFiles/worker.csv'
+import sys, os
+FILENAME = '../DataFiles/worker.csv'
 
 
 class Worker():
 
     def __init__(self):
-        # self.get_flight_route_from_file()
-        pass
+        self.__dictList = []
 
     def get_worker_from_file(self):
         """Get worker from file in a list of dictionaries"""
@@ -27,7 +26,7 @@ class Worker():
             orderedDict = self.convert_to_dict(aList)
             self.__dictList.append(orderedDict)
             newList = []
-            newList.append(orderedDict['social security number'])
+            #newList.append(orderedDict['social security number']) BÆTIR VIÐ SSN FREMST???
             [newList.append(i) for i in aList]
             csvWriter.writerow(newList)
 
@@ -37,32 +36,33 @@ class Worker():
             fieldnames = ['social security number'
                         ,'name'
                         ,'position'
+                        ,'rank'
+                        ,'plane licence'
                         ,'address'
                         ,'phone'
                         ,'cellphone'
                         ,'email'
                         ,'active'
-                        ,'available'
-                        ,'plane licence']
+                        ,'available']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             for dictionary in self.__dictList:
                 writer.writerow(dictionary)
 
-    def convert_to_dict_no_id(self, aList):
-        """Function converts list to dict but doesn't
-        generate an id since ID should be provided in argument list"""
+    def convert_to_dict(self, aList):
+        """Function converts list to dict"""
         orderedDict = {}
         orderedDict['social security number'] = aList[0]
         orderedDict['name'] = aList[1]
         orderedDict['position'] = aList[2]
-        orderedDict['address'] = aList[3]
-        orderedDict['phone'] = aList[4]
-        orderedDict['cellphone'] = aList[5]
-        orderedDict['email'] = aList[6]
-        orderedDict['active'] = aList[7]
-        orderedDict['available'] = aList[8]
-        orderedDict['plane licence'] = aList[9]
+        orderedDict['rank'] = aList[3]
+        orderedDict['plane licence'] = aList[4]
+        orderedDict['address'] = aList[5]
+        orderedDict['phone'] = aList[6]
+        orderedDict['cellphone'] = aList[7]
+        orderedDict['email'] = aList[8]
+        orderedDict['active'] = aList[9]
+        orderedDict['available'] = aList[10]
         return orderedDict
 
     def update_data_in_file(self, aList):
@@ -70,7 +70,17 @@ class Worker():
         and writes the changes to file"""
         for index, dictionary in enumerate(self.__dictList):
             for key, value in dictionary.items():
-                if key == 'flight route id':
+                if key == 'social security number':
                     if value == aList[0]:
                         self.__dictList[index][aList[1]] = aList[2]
                         self.write_dictList_to_file()
+
+
+writeList = ["9999999999","Elizabeth Mcfadden","Cabincrew","Flight Attendant","N/A","Fellsmúli 35","8998835","8998835","test@test.com","True","True"]
+updateList = ['9999999999', 'position', 'Dick']
+worker = Worker()
+
+worker.get_worker_from_file()
+
+worker.write_worker_to_file(writeList)
+worker.update_data_in_file(updateList)
