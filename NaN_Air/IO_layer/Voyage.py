fff5d3c76e4c76621a736e34fb99451e0944f259
Dinkus <robertsnaer901@gmail.com>
2019-12-06 11:34:27 +0000

    Started makeing changes on Voyage

--- NaN_Air/IO_layer/Voyage.py
+++ NaN_Air/IO_layer/Voyage.py
@@ -1,21 +1,19 @@
 class CreateVoyage:
     def __init__(self, dictionary):
-        self.__myDictionary = {}
-        self.__myDictionary["Voyage Id"] = dictionary["voyage id"]
-        self.__myDictionary["Flight out ID"] = dictionary["Flight out ID"]
-        self.__myDictionary["Flight back ID"] = dictionary["Flight back ID"]
-        self.__myDictionary["Main Pilot"] = dictionary["Main Pilot"]
-        self.__myDictionary["Assisting Pilot"] = dictionary["Assisting Pilot"]
-        self.__myDictionary["Main Flight Attendant"] = dictionary["Main Flight Attendant"]
-        self.__myDictionary["Flight Attendants"] = dictionary["Flight Attendants"]
-        self.__myDictionary["Flight Route ID"] = dictionary["Flight route ID"]
-        self.__myDictionary["Departure from Is"] = dictionary["departure from is"]
-        self.__myDictionary["Departure to Is"] = dictionary["departure to is"]
+        self.__myDictionary = dictionary
+        self.voyageId = dictionary["Voyage ID"]
+        self.flightOutID = dictionary["Flight out ID"]
+        self.flightBackID = dictionary["Flight back ID"]
+        self.mainPilot = dictionary["Main pilot"]
+        self.assistingPilot = dictionary["Assisting pilot"]
+        self.mainFlightAttendant = dictionary["Main flight attendant"]
+        self.flightAttendants = dictionary["Flight attendants"]
+        self.flightRouteID = dictionary["Flight route ID"]
+        self.departureFromIs = dictionary["Departure from IS"]
+        self.departureToIs = dictionary["Departure to IS"]
 
     def __str__(self):
         returnString = []
         for key, val in self.__myDictionary.items():
             returnString.append((key + ": " + val))
         return "\n".join(returnString)
-
-
